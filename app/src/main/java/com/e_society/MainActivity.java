6c064ad8e77ca41cd43d774061ab76031919c9ac
janvi18 <sjanvi9333@gmail.com>
2022-07-12 23:17:04 +0530

    first

---
+++ app/src/main/java/com/e_society/MainActivity.java
@@ -0,0 +1,31 @@
+package com.e_society;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    int time = 5000;
+    GifImageView gifImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gifImageView = findViewById(R.id.gif);
+        gifImageView.setGifImageResource(R.drawable.enhomes);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },time);
+    }
+}
